162a91d127a4d94be9214449329a8b469a44a8ca
Muhammad Saikat <smsaikat000@gmail.com>
2023-12-21 22:17:47 +0600

    added route to post todo

--- index.js
+++ index.js
@@ -22,28 +22,35 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-      // Connect the client to the server (uncomment this line)
-      await client.connect();
-  
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      const Users = client.db("TASK").collection("Users");
-      console.log(
-        "Pinged your deployment. You successfully connected to MongoDB!"
-      );
-  
-      app.post("/postUser", async (req, res) => {
-        const data = await req.body;
-        const result = await Users.insertOne(data);
-        res.send(result);
-      });
-    } finally {
-      // Ensures that the client will close when you finish/error
+  try {
+    // Connect the client to the server (uncomment this line)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    const Users = client.db("TASK").collection("Users");
+    const TODO = client.db("TASK").collection("TODO");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    // * post user into database
+    app.post("/postUser", async (req, res) => {
+      const data = await req.body;
+      const result = await Users.insertOne(data);
+      res.send(result);
+    });
+    // * post user todo data 
+    app.post(`/addTodo`,async(req,res)=>{
+        const data=req.body 
+        const result=await TODO.insertOne(data)
+        res.send(result)
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
     //   await client.close();
-    }
   }
-  
+}
+
 run().catch(console.dir);
 
 app.listen(port);
